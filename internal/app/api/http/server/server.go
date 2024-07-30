277b5969c6d63369dd979ecc599816a0cb12ad6a
yatoenough <yatoenough@gmail.com>
2024-07-30 14:21:48 +0200

    added not found route handler

--- internal/app/api/http/server/server.go
+++ internal/app/api/http/server/server.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -11,7 +14,17 @@ func New() *echo.Echo {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	RegisterRoutes(e.Group("/api"))
+	registerRoutes(e)
 
 	return e
+}
+
+func registerRoutes(e *echo.Echo) {
+	e.RouteNotFound("*", func(c echo.Context) error {
+		return c.JSON(http.StatusNotFound, map[string]any{
+			"code":      http.StatusNotFound,
+			"message":   "Not found",
+			"timestamp": time.Now(),
+		})
+	})
 }
