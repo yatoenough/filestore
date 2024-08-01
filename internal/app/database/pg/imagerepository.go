22fd35264b1bb924aaf6841bfcd7048f1700a1fc
yatoenough <yatoenough@gmail.com>
2024-08-01 09:45:42 +0200

    implemented find functions in image repository

--- internal/app/database/pg/imagerepository.go
+++ internal/app/database/pg/imagerepository.go
@@ -9,15 +9,38 @@ type ImageRepository struct {
 }
 
 func (r *ImageRepository) Create(i *model.Image) (*model.Image, error) {
-	if err := r.storage.db.QueryRow(
-		"INSERT INTO images (original_name, store_name, path, ext) VALUES ($1, $2, $3, $4) RETURNING id",
-		i.OriginalName,
-		i.StoreName,
-		i.Path,
-		i.Extension,
-	).Scan(&i.ID); err != nil {
+	res, err := r.storage.db.NamedQuery(
+		"INSERT INTO images (original_name, store_name, path, ext) VALUES (:original_name, :store_name, :path, :ext) RETURNING id",
+		&i,
+	)
+
+	if err != nil {
 		return nil, err
 	}
 
+	if res.Next() {
+		res.Scan(&i.ID)
+	}
+
 	return i, nil
+}
+
+func (r *ImageRepository) FindAll() (*[]model.Image, error) {
+	var images []model.Image
+	err := r.storage.db.Select(&images, "SELECT id, original_name, store_name, path, ext FROM images")
+	if err != nil {
+		return nil, err
+	}
+
+	return &images, nil
+}
+
+func (r *ImageRepository) FindById(id int64) (*model.Image, error) {
+	var image model.Image
+	err := r.storage.db.Get(&image, "SELECT id, original_name, store_name, path, ext FROM images WHERE id = $1", id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &image, nil
 }
--- internal/app/model/image.go
+++ internal/app/model/image.go
@@ -1,9 +1,9 @@
 package model
 
 type Image struct {
-	ID int64
-	OriginalName string
-	StoreName string
-	Path string
-	Extension string
+	ID           int64  `db:"id"`
+	OriginalName string `db:"original_name"`
+	StoreName    string `db:"store_name"`
+	Path         string `db:"path"`
+	Extension    string `db:"ext"`
 }
